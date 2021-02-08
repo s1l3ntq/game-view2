23188cd5baad6302ba30c08006f8f7aeb410e3ac
s1l3ntq <qimiller89@gmail.com>
2021-02-08 01:47:24 -0600

    Created sessions controller and views, set up login and signup routes and forms.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 get '/signup' => 'users#new'
 post '/signup' => 'users#create'
 
-get '/login', => 'sessions#new'
-post '/login'. => 'sessions#create'
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
 delete '/logout' => 'sessions#destroy'
 
 resources :games
